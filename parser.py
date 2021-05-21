c544b3e25beea61d9b4d884fe96cbc8147e649f0
Wictor Girardi <wictor.girardi@gmail.com>
2021-05-21 19:20:51 -0300

    Task done

--- parser.py
+++ parser.py
@@ -7,9 +7,27 @@ class Symbol(NamedTuple):
     value: str
 
 
-grammar = Lark(
-    r"""
-start : "implemente-aqui!"
+grammar = Lark(r"""
+    ?start : expr+
+    ?expr  : atom
+           | lista
+           | quoted 
+    quoted : "'" expr
+    lista   : "(" expr+ ")"
+    ?atom  : STRING -> string
+           | SYMBOL -> symbol
+           | NUMBER -> number
+           | BOOLEAN -> boolean
+           | NAME -> name
+           | CHAR -> char
+    STRING : /"[^"\\]*"/
+    SYMBOL: /[-+=\/*!@$^&~<>?]+/
+    NUMBER : /-?\d+(\.\d+)?/
+    BOOLEAN: /\#t|\#nil/
+    NAME   : /[a-zA-Z][-?\w]*/
+    CHAR   : /\#\\\w+/
+    %ignore /\s+/
+    %ignore /;[^\n]*/
 """)
 
 
@@ -26,3 +44,33 @@ class LispyTransformer(InlineTransformer):
         "space": " ",
         "tab": "\t",
     }
+
+    def number(self, token):
+        return float(token)
+    
+    def string(self, token):
+        return eval(token)
+
+    def symbol(self, token):
+        return Symbol(token)
+
+    def name(self, token):
+        return Symbol(token)
+        
+    def lista(self, *args):
+        return list(args)
+
+    def boolean(self, token):
+        return True if token.value == "#t" else False
+
+    def quoted(self, token):
+        return [Symbol('quote'), token]
+
+    def start(self, *args):
+        array = list(args)
+        array.insert(0, Symbol('begin'))
+        return array
+
+    def char(self, token):
+        token = token.split('#\\')[-1]
+        return token if token.lower() not in self.CHARS else self.CHARS[token.lower()]
